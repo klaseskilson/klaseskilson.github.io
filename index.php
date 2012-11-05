95c0f30f6009fa4b90c3e5b9c82b9f1bfee94b3a
Klas Eskilson <klas.eskilson@gmail.com>
2012-11-05 21:07:28 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+//index.php - klasse
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Klas Eskilson</title>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<style type="text/css" media="screen, projection">
+			@import url(http://reset5.googlecode.com/hg/reset.min.css);
+			@import url(http://fonts.googleapis.com/css?family=Anaheim|Strait);
+
+			body {
+				font-family: sans-serif;
+				height: 100%;
+				width: 100%;
+				font-size: 100%;
+			}
+
+			#main {
+				width: 50%;
+				height: 50%;
+				margin: 10% auto;
+			}
+
+			h1 {
+				font: 220% Anaheim, sans-serif;
+				border-bottom: 2px solid #eee;
+				margin: 0 0 20px;
+				line-height: 100%;
+			}
+
+			p {
+				font: 1.3em Strait, sans-serif;
+			}
+
+			a {
+				color: #777;
+				text-decoration: none;
+				border-bottom: 1px dashed #ccc;
+			}
+
+				a:hover {
+					border-color: #222;
+					color: #222;
+				}
+		</style>
+	</head>
+	<body>
+		<div id="main">
+			<h1>Klas Eskilson</h1>
+			<p>Hej! Jag heter Klas. Du når mig lättast här: <a href="mailto:hej@klaseskilson.se" target="_blank" title="Maila mig!">hej@klaseskilson.se</a>.</p>
+		</div>
+	</body>
+</html>
