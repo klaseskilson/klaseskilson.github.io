57d586afb69b8d8e3bc43804324ff1d930981abd
Klas Eskilson <klas.eskilson@gmail.com>
2012-11-05 22:11:02 +0100

    change index.php to a more adaptive base

--- index.php
+++ index.php
@@ -8,50 +8,35 @@
 	<head>
 		<title>Klas Eskilson</title>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-		<style type="text/css" media="screen, projection">
-			@import url(http://reset5.googlecode.com/hg/reset.min.css);
-			@import url(http://fonts.googleapis.com/css?family=Anaheim|Strait);
 
-			body {
-				font-family: sans-serif;
-				height: 100%;
-				width: 100%;
-				font-size: 100%;
-			}
+		<!-- mobile friendly stuff -->
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
 
-			#main {
-				width: 50%;
-				height: 50%;
-				margin: 10% auto;
-			}
-
-			h1 {
-				font: 220% Anaheim, sans-serif;
-				border-bottom: 2px solid #eee;
-				margin: 0 0 20px;
-				line-height: 100%;
-			}
-
-			p {
-				font: 1.3em Strait, sans-serif;
-			}
-
-			a {
-				color: #777;
-				text-decoration: none;
-				border-bottom: 1px dashed #ccc;
-			}
-
-				a:hover {
-					border-color: #222;
-					color: #222;
-				}
-		</style>
+		<!-- adaptive and regular style -->
+		<link rel="stylesheet" href="css/main.css" media="all">
+		<link rel="stylesheet" href="css/grid.css" media="all">
+		<link rel="stylesheet" href="css/cols.css" media="all">
 	</head>
 	<body>
 		<div id="main">
 			<h1>Klas Eskilson</h1>
 			<p>Hej! Jag heter Klas. Du når mig lättast här: <a href="mailto:hej@klaseskilson.se" target="_blank" title="Maila mig!">hej@klaseskilson.se</a>.</p>
 		</div>
+<?php
+/*
+	<!-- JavaScript at the bottom for fast page loading -->
+
+	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
+
+	<!--[if (lt IE 9) & (!IEMobile)]>
+	<script src="js/selectivizr-min.js"></script>
+	<![endif]-->
+*/
+?>
+
 	</body>
 </html>
