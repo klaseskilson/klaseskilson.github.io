e59084beea57183721ff275797cc4914f119cbe1
Klas Eskilson <klas.eskilson@gmail.com>
2012-12-20 23:21:12 +0100

    ny version!

--- index.php
+++ index.php
@@ -1,35 +1,81 @@
-<?php
-
-//index.php - klasse
-
-?>
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="utf-8" />
 		<title>Klas Eskilson</title>
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<!-- mobile friendly stuff -->
-		<meta name="HandheldFriendly" content="True">
-		<meta name="MobileOptimized" content="320">
-		<meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
-
-		<!-- adaptive and regular style -->
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
 		<link rel="stylesheet" href="css/main.css" media="all">
-		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Anaheim|Strait" media="all">
+		<link href='http://fonts.googleapis.com/css?family=Arvo|Quicksand:400,700' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
-		<div id="main">
-			<header>
-				<h1>Klas Eskilson</h1>
+		<div class="container-fluid" id="main">
+			<section id="intro" class="background">
 				<p>
-					Hej! Jag heter Klas. Ibland gör jag saker på nätet. Gärna på <a href="http://femtjugo.se" target="_blank">520</a> som jag var med och startade.
+					Hej! Jag heter Klas Eskilson. Ibland gör jag saker på nätet.
 				</p>
-				<p>
-					Om du vill nå mig så gör du det lättast på <a href="mailto:hej@klaseskilson.se" target="_blank" title="Maila mig!">hej@klaseskilson.se</a> eller <a href="http://twitter.com/eskilicious" target="_blank" title="Titta på Twitter då!">@Eskilicious</a>.
+				<p class="clearfix">
+					Om du vill nå mig så gör du det lättast på
+					<a href="mailto:hej@klaseskilson.se" target="_blank" title="Maila mig!">hej@klaseskilson.se</a> eller
+					på <a href="http://twitter.com/eskilicious" target="_blank" title="Titta på Twitter då!">@Eskilicious</a>.
 				</p>
-			</header>
-		</div>
-		<img src="img/klasselofi.jpg" id="klasse" alt="Klas Eskilson" />
+				<p class="bottom hidden-phone">
+					<span id="continue">Fortsätt <i class="icon-hand-down"></i></span>
+				</p>
+			</section> <!-- #intro -->
+			<section id="content">
+				<h2>
+					Om mig
+				</h2>
+				<div class="row-fluid">
+					<div class="span8">
+						<p>
+							Just nu läser jag till <a href="http://medieteknik.nu">civilingenjör
+							i medieteknik</a> på Linköpings Universitet i Norrköping.
+						</p>
+						<p>
+							Jag finns på diverse ställen på nätet, bland annat på
+							<a href="http://twitter.com/eskilicous"><i class="icon-comment"></i> twitter</a>,
+							<a href="http://flickr.com/klaseskilson"><i class="icon-camera"></i> flickr</a> och
+							<a href="http://github.com/klaseskilson"><i class="icon-edit"></i> github</a>.
+						</p>
+						<p>
+							Det senaste jag gjort, eller varit med i, här på nätet är
+							<a href="http://sommansägerinorrköping.nu">sommansägerinorrköping.nu</a>,
+							<a href="http://isandediskant.nu">Isande diskant</a>,
+							<a href="http://lithekoll.nu">LiTHekoll</a> och
+							<a href="http://femtjugo.se">520</a>.
+						</p>
+					</div>
+					<div class="span4">
+						<p>
+							<img src="img/klas<?php echo mt_rand(1,1); ?>.jpg" alt="Klas Eskilson" />
+						</p>
+					</div>
+				</div>
+			</section> <!-- #info -->
+			<section id="footer" class="wrapper">
+				<div class="row-fluid">
+					<div class="span4">
+						<p>
+							Maila mig på <a href="mailto:hej@klaseskilson.se" target="_blank" title="Maila mig!">hej@klaseskilson.se</a>.
+						</p>
+					</div>
+					<div class="span4 offset4">
+						<p>
+							På den här sidan har jag använt mig av <a href="http://twitter.github.com/bootstrap/">Bootstrap</a>.
+						</p>
+					</div>
+				</div>
+			</section> <!-- #footer -->
+		</div> <!-- #main -->
+
+		<!-- Ladda in jquery och bootstrap -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+		<script src="js/klasse.js"></script>
 	</body>
 </html>
