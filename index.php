9093ddc6930e0ab20d2be5e0e771d1b9a2fcfec1
Klas Eskilson <klas.eskilson@gmail.com>
2013-09-26 00:42:19 +0200

    Prep for new layout

--- index.php
+++ index.php
@@ -6,76 +6,35 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<!-- Bootstrap -->
-		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-		<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
-		<link rel="stylesheet" href="css/main.css" media="all">
-		<link href='http://fonts.googleapis.com/css?family=Arvo|Quicksand:400,700' rel='stylesheet' type='text/css'>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+
+		<!-- Personal design -->
+		<!-- <link rel="stylesheet" href="css/style.css" media="all"> -->
 	</head>
 	<body>
-		<div class="container-fluid" id="main">
-			<section id="intro" class="background">
-				<p>
-					Hej! Jag heter Klas Eskilson. Ibland gör jag saker på nätet.
-				</p>
-				<p class="clearfix">
-					Om du vill nå mig så gör du det lättast på
-					<a href="mailto:hej@klaseskilson.se" target="_blank" title="Maila mig!">hej@klaseskilson.se</a> eller
-					på <a href="http://twitter.com/eskilicious" target="_blank" title="Titta på Twitter då!">@Eskilicious</a>.
-				</p>
-				<p class="bottom hidden-phone">
-					<a href="#about" class="scroll" id="continue">Fortsätt <i class="icon-hand-down"></i></a>
-				</p>
-			</section> <!-- #intro -->
-			<section id="content">
-				<h2 id="about">
-					Om mig
-				</h2>
-				<div class="row-fluid">
-					<div class="span8">
-						<p>
-							Just nu läser jag till <a href="http://medieteknik.nu">civilingenjör
-							i medieteknik</a> på Linköpings Universitet i Norrköping.
-						</p>
-						<p>
-							Jag finns på diverse ställen på nätet, bland annat på
-							<a href="http://twitter.com/eskilicious"><i class="icon-comment"></i> twitter</a>,
-							<a href="http://flickr.com/klaseskilson"><i class="icon-camera"></i> flickr</a> och
-							<a href="http://github.com/klaseskilson"><i class="icon-edit"></i> github</a>.
-						</p>
-						<p>
-							Det senaste jag gjort, eller varit med i, här på nätet är
-							<a href="http://sommansägerinorrköping.nu">sommansägerinorrköping.nu</a>,
-							<a href="http://isandediskant.nu">Isande diskant</a>,
-							<a href="http://lithekoll.nu">LiTHekoll</a> och
-							<a href="http://femtjugo.se">520</a>.
-						</p>
-					</div>
-					<div class="span4">
-						<p>
-							<img src="img/klas<?php echo mt_rand(1,1); ?>.jpg" alt="Klas Eskilson" />
-						</p>
-					</div>
+		<div class="container" id="main">
+			<div class="row" id="header">
+				<div class="col-sm-4">
+					Klas
 				</div>
-			</section> <!-- #info -->
-			<section id="footer" class="wrapper">
-				<div class="row-fluid">
-					<div class="span4">
-						<p>
-							Maila mig på <a href="mailto:hej@klaseskilson.se" target="_blank" title="Maila mig!">hej@klaseskilson.se</a>.
-						</p>
-					</div>
-					<div class="span4 offset4">
-						<p>
-							På den här sidan har jag använt mig av <a href="http://twitter.github.com/bootstrap/">Bootstrap</a>.
-						</p>
-					</div>
+				<div class="col-sm-8">
+					Info
 				</div>
-			</section> <!-- #footer -->
+			</div>
 		</div> <!-- #main -->
 
-		<!-- Ladda in jquery och bootstrap -->
+		<!-- Load jQuery -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 		<script src="js/klasse.js"></script>
 		<script type="text/javascript">
 			var _gaq = _gaq || [];
